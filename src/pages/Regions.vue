869351a7a851d56dd3d0cf86d080a84581b3cd1c
oswago hesbon <oswago76@gmail.com>
2021-06-23 18:13:14 +0300

    .

--- src/pages/Regions.vue
+++ src/pages/Regions.vue
@@ -1,18 +1,12 @@
 <template>
   <dashboard_layout page_title="">
-    <GoBack />
-      
-  <dashboard_layout page_title="">      
-      <router-link
-        v-if="$routerHistory.hasPrevious()"
-        :to="{ path: $routerHistory.previous().path }">
-        GO BACK
-    </router-link>
+
     <!-- REGIONS -->
 
      <div class="grid col-span-2 items-center rounded-lg shadow-xs dark:bg-gray-800 py-3">          
         <div class="flex justify-between">
-          <div class=""></div>             
+               
+          <div class=""><GoBack /></div>             
           <div class="">
             <button class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 px-4 py-2 text-sm font-medium leading-5 bg-blue-100 text-blue-500 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline-purple capitalize flex" @click="openAddRegionForm">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
@@ -79,24 +73,24 @@
                     Add Region
                   </h3>
                   <div class="mt-2 ">
-                        <label class="block mt-4 text-sm w-full">
-                            <span class="text-gray-700  font font-semibold dark:text-gray-400">Region Name</span>
-                            <!-- focus-within sets the color for the icon when input is focused -->
-                            
-                            <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-                                <input type="text"
-                                        class="block w-full border-2  border-gray-200 rounded-sm p-2 pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                                        placeholder="e.g. Arusha"
-                                        name="name"
-                                        validation="required"
-                                        v-model="form.name"
-                                />
-                                <div class="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
-                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"></path></svg>
-                                </div>
-                            </div>
-                        </label>                       
-                </div>
+                      <label class="block mt-4 text-sm w-3/2">
+                          <span class="text-gray-700  font font-semibold dark:text-gray-400">Region Name</span>
+                          <!-- focus-within sets the color for the icon when input is focused -->
+                          
+                          <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+                              <input type="text"
+                                      class=" border-2  border-gray-200 rounded-sm p-2 pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                                      placeholder="e.g. Arusha"
+                                      name="name"
+                                      validation="required"
+                                      v-model="form.name"
+                              />
+                              <div class="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
+                                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"></path></svg>
+                              </div>
+                          </div>
+                      </label>                       
+                  </div>
                 </div>
               </div>
             </div>
@@ -205,15 +199,10 @@ export default {
   },
   methods: {
     
-    ...mapActions(['postRegion','fetchRegionById','updateRegionById','deleteRegionById']),
-    getCurrentRegions(){
-        let selectedRegions = []
-        selectedRegions = this.allRegions.filter(region=>{
-            return region.country.id ===  this.$route.params.countryId
-        });
-        this.filteredRegions = selectedRegions;
+    ...mapActions(['fetchRegions','postRegion','fetchRegionById','updateRegionById','deleteRegionById']),
+ 
         
-    },
+   
     openAddRegionForm(){
       this.add_region = true      
     },
@@ -263,11 +252,28 @@ export default {
             
         )
     },   
+    // getting regions
+    getCurrentRegions(){
+      let selectedRegions = []
+      selectedRegions = this.allRegions.filter(region=>{
+          return region.country.id ===  this.$route.params.countryId
+      });
+      this.filteredRegions = selectedRegions;
+    },
+     
+    getRegions() {
+      this.$store.dispatch('fetchRegions').then(resp => {
+        this.regions = resp;
+      }).catch(err => {
+        console.log(err);
+      })
+    },
     // end of regions
       
   },
   mounted() {    
     this.getCurrentRegions()
+    this.getRegions()
     },
   computed: {
       ...mapGetters(['allRegions','getErrorMessage'])
