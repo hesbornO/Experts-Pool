09f0ba2ad3963578f86aa5da49f52d413533e3c6
hesborn <oswago54@gmail.com>
2021-11-15 22:21:12 +0300

    changed login to formulate form

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -17,10 +17,10 @@
               <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                 Login
               </h1>
-              <form @submit.prevent="userLogin">
+              <FormulateForm  v-model="form" @submit="userLogin">
                 <label class="block text-sm ">
                   <span class="text-gray-700 dark:text-gray-400">Username</span>
-                  <input v-model="username" type="text"
+                  <FormulateInput name="username" type="text"
                         placeholder="Jane Doe" class="mt-1 block w-full rounded-md border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                         required
                         />
@@ -28,11 +28,11 @@
                 <label class="block mt-4 text-sm">
                   <span class="text-gray-700 dark:text-gray-400">Password</span>
                   <span class="flex pt-3">
-                    <input v-model="password"
+                    <FormulateInput name="password"
                           class="block w-full mt-1 text-sm rounded-md dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray "
                           placeholder="**************"
                           :type="passwordFieldType"
-                          validation="required|between:6,20,length"
+                          validation="required|between:5,20,length"
                           />
                     <button type="password" @click="switchVisibility" class="h-2/3 rounded-md bg-purple-300 pt-2" title="View/Hide password">
                           <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" v-if="passwordFieldType==='password'" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
@@ -42,7 +42,6 @@
                 </label>
 
                 <span class="text-xs text-red-400 mt-6" v-for="(key, index) in Object.keys(getErrorMessage)" :key="index">
-
                   <span v-if="key!=='detail'">
                     <span v-for="(detail_key, index) in Object.keys(getErrorMessage[key])" :key="index">
                       <span class="capitalize">{{key}}</span> : {{getErrorMessage[key][detail_key]}}
@@ -51,15 +50,13 @@
                   <span v-else>
                     {{getErrorMessage.detail}}
                   </span>
-
-
                 </span>
 
                 <!-- You should use a button here, as the anchor is only used for the example  -->
-                <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" @click="userLogin">
-                  Log in
+                <button :class="['block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple disabled cursor-not-allowed',submitting?'disabled cursor-not-allowed':'']" type="submit" title="Click to login">
+                  {{submitting?'Logging in...':'Log In'}}
                 </button>
-              </form>
+              </FormulateForm>
               <hr class="my-8"/>
               <p class="mt-4">
                 <router-link class="w-full" to="/forgot-password"><span
@@ -84,26 +81,27 @@ import {mapActions, mapGetters} from 'vuex'
 export default {
   data() {
     return {
-      username: '',
-      password: '',
+      form:{
+        username: '',
+        password: '',
+      },
       passwordFieldType:'password',
-      showSidebar: false
+      showSidebar: false,
+      submitting: false
     }
   },
   methods: {
     ...mapActions(['login']),
     userLogin() {
+      console.log('submitting: ',this.submitting)
+      this.submitting = true
       
-      let payload = {
-        username: this.username,
-        password: this.password
-      }
       // eslint-disable-next-line no-unused-vars
-      this.login(payload).then(resp => {
+      this.login(this.form).then(resp => {
         let user_assigned_roles_string = localStorage.getItem('roles')
         let user_assigned_roles = []
         if (user_assigned_roles_string === ''){
-         //if the route has no specified roles, default to rde
+          //if the route has no specified roles, default to rde
           user_assigned_roles.push('rde')
         }else{
           // console.log('assigned roles:', user_assigned_roles_string)
@@ -118,7 +116,7 @@ export default {
       }).catch(err => {
         console.log(err)
       })
-
+      this.submitting = false
     },
     switchVisibility(e){
       e.preventDefault();     
