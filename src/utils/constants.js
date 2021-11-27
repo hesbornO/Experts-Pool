2c9236f0c52e54d0bbae418c3483d7fae40ff680
SamK <samuelkyama102@gmail.com>
2021-11-27 19:02:19 +0300

    self profile changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // let baseUrl = "http://35.208.99.81:84/";
-let baseUrl = " http://f723-197-232-113-104.ngrok.io/";
-// let baseUrl = "http://localhost:8000/";
+// let baseUrl = " http://f723-197-232-113-104.ngrok.io/";
+let baseUrl = "http://localhost:8000/";
 let api_url = baseUrl + 'api/'
 let app_name = 'RDE'
 export {
