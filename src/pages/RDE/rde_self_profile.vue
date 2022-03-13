b82fca76b7b901c225fde5472cc823345785cb2c
hesborn <oswago54@gmail.com>
2022-03-14 00:19:43 +0300

    .

--- src/pages/RDE/rde_self_profile.vue
+++ src/pages/RDE/rde_self_profile.vue
@@ -370,7 +370,7 @@
                     <button @click="togglePdfDisplay('fetchCV','viewPdf')" 
                       class="hover-animation px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-yellow-400 border border-transparent rounded-lg active:bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:shadow-outline-blue" 
                       >
-                      <span>View uploaded CV</span>
+                      <span>View CV</span>
                     </button>
                   </span>
                   <span>
