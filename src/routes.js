292da1c3094af438a5ea9e8949f283896371792a
hesborn <oswago54@gmail.com>
2022-04-21 15:09:30 +0300

    accept deployment

--- src/routes.js
+++ src/routes.js
@@ -767,7 +767,7 @@ const routes = [
         component: rde_deployments,
         showInLeftBar: true,
         icon: '<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>',
-        verboseName: 'Deployments',
+        verboseName: 'MyDeployments',
         props: x => {
             return {
                 vuex_data_action: 'fetchRDEById',
