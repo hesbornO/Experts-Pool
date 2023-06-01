2f88863ff55df46d7e741ea034a0861cf39fb05e
hesborn <oswago54@gmail.com>
2023-06-01 16:46:19 +0300

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 // let baseUrl = "http://35.208.99.81:84/";
-let baseUrl = "http://rdeapi.eachq.org/";
-// let baseUrl = "http://localhost:8000/";
+let baseUrl = "http://localhost:8000/";
 let api_url = baseUrl + 'api/'
 let app_name = 'RDE'
 export {
